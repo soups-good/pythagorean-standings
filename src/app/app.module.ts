d56e1b7dae6f54ed20a334d8532e3dbf1261afe2
DWT <dwthomas008@gmail.com>
2023-07-29 18:54:50 -0500

    feat(setup): initial framework of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StandingsPageComponent } from './standings-page/standings-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [StandingsPageComponent, HeaderComponent, FooterComponent],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
 	providers: [],
-	bootstrap: [AppComponent],
+	bootstrap: [StandingsPageComponent, HeaderComponent, FooterComponent],
 })
 export class AppModule {}
---
+++ src/app/standings-page/standings-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-standings-page',
+	templateUrl: './standings-page.component.html',
+	styleUrls: [],
+})
+export class StandingsPageComponent {
+	title = 'Pythagorean Standings';
+}
