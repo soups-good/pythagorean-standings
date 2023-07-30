f070181a11c4f8c4be0174452dc9b8ac3313d305
DWT <dwthomas008@gmail.com>
2023-07-29 19:44:54 -0500

    feat(card): initial grid setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,8 +14,19 @@ import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
 	declarations: [StandingsPageComponent, HeaderComponent, FooterComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatDividerModule,
+		MatButtonModule,
+		MatProgressBarModule,
+	],
 	providers: [],
 	bootstrap: [StandingsPageComponent, HeaderComponent, FooterComponent],
 })
 export class AppModule {}
+// My goal is to throw a generic material header in, and then maybe do some ng templates with the data and the mat cards?
+// It'd be fun to have a toggle on each division's card that lets you switch between pythag and actual standings
--- src/app/standings-page/standings-page.component.ts
+++ src/app/standings-page/standings-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-standings-page',
 	templateUrl: './standings-page.component.html',
-	styleUrls: [],
+	styleUrls: ['./standings-page.component.scss'],
 })
-export class StandingsPageComponent {
-	title = 'Pythagorean Standings';
+export class StandingsPageComponent implements OnInit {
+	data: string[] = [];
+
+	public ngOnInit(): void {
+		this.data.push('');
+	}
 }
